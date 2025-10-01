dccd4dad0de8c1b625dbfa3c1805f4e1d28a5a76
Toodie <lucas.vtodt@gmail.com>
2025-10-01 07:10:57 -0300

    feat: load Json file and create Deck object

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,24 @@
+import type { IDeckStructure } from "./interfaces/IDeckStructure";
+import { Deck } from "./models/deck";
 import "./style.css";
 
-console.log("Hello World!");
+const fileInput = document.getElementById("file-input") as HTMLElement;
+const loadDeckBtn = document.getElementById("load-deck-btn") as HTMLElement;
+let deck = null;
+
+fileInput.addEventListener("change", (event: Event) => {
+  const files = (event.target as HTMLInputElement).files!;
+  const file = files[0];
+  const reader = new FileReader();
+
+  if (!file) return;
+  if (file.type != "application/json") return;
+
+  reader.onloadend = () => {
+    const data = JSON.parse(reader.result as string) as IDeckStructure;
+    deck = new Deck(data);
+  };
+  reader.readAsText(file);
+});
+
+loadDeckBtn.addEventListener("click", (_) => fileInput.click());
