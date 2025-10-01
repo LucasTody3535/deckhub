4fdcfd75d245b1d50d9fc90c4bda8a240f88fad6
Toodie <lucas.vtodt@gmail.com>
2025-10-01 09:17:03 -0300

    style: animate button after deck data is loaded

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,25 @@ import type { IDeckStructure } from "./interfaces/IDeckStructure";
 import { Deck } from "./models/deck";
 import { Hand } from "./models/hand";
 import "./style.css";
+import "./animations.css";
 
 const fileInput = document.getElementById("file-input") as HTMLElement;
 const loadDeckBtn = document.getElementById("load-deck-btn") as HTMLElement;
 let deck = null;
 let hand = null;
 let data = null;
+
+let didAlreadyAnimationButton = false;
+
+function prepareUI() {
+  if (!didAlreadyAnimationButton) {
+    const animationClass = "load-deck-btn-when-game-started";
+    loadDeckBtn.classList.add(animationClass);
+    loadDeckBtn.addEventListener("animationend", () => {
+      didAlreadyAnimationButton = true;
+    });
+  }
+}
 
 fileInput.addEventListener("change", (event: Event) => {
   const files = (event.target as HTMLInputElement).files!;
@@ -21,6 +34,7 @@ fileInput.addEventListener("change", (event: Event) => {
     data = JSON.parse(reader.result as string) as IDeckStructure;
     deck = new Deck(data);
     hand = new Hand(deck.drawInitialQuantity());
+    prepareUI();
   };
   reader.readAsText(file);
 });
