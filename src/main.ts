9011e798ff5ab052b73be7277fd2dca0f7038be0
Toodie <lucas.vtodt@gmail.com>
2025-10-01 09:51:44 -0300

    style: display deck UI after button animation

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { Hand } from "./models/hand";
 import "./style.css";
 import "./animations.css";
 
+const appContainer = document.getElementById("app") as HTMLElement;
 const fileInput = document.getElementById("file-input") as HTMLElement;
 const loadDeckBtn = document.getElementById("load-deck-btn") as HTMLElement;
 let deck = null;
@@ -12,12 +13,23 @@ let data = null;
 
 let didAlreadyAnimationButton = false;
 
+function createDeckUI() {
+  let container = document.createElement("div");
+  let text = document.createElement("p");
+  text.innerText = "Deck";
+  text.classList.add("deck-ui-text");
+  container.classList.add("deck-ui");
+  container.appendChild(text);
+  appContainer.appendChild(container);
+}
+
 function prepareUI() {
   if (!didAlreadyAnimationButton) {
     const animationClass = "load-deck-btn-when-game-started";
     loadDeckBtn.classList.add(animationClass);
     loadDeckBtn.addEventListener("animationend", () => {
       didAlreadyAnimationButton = true;
+      createDeckUI();
     });
   }
 }
