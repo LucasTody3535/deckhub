53baf8d63990b366c53965ae115a2e0a2ff3cf8f
Toodie <lucas.vtodt@gmail.com>
2025-10-02 14:46:26 -0300

    feat: impl class DrawEffect

---
+++ src/models/draw_effect.ts
@@ -0,0 +1,19 @@
+import type { IEffect } from "../interfaces/IEffect";
+import type { Deck } from "./deck";
+import type { Hand } from "./hand";
+
+export class DrawEffect implements IEffect {
+  private deck: Deck;
+  private hand: Hand;
+
+  constructor(deck: Deck, hand: Hand) {
+    this.deck = deck;
+    this.hand = hand;
+  }
+
+  apply(): void {
+    const cardDrawed = this.deck!.drawCard();
+    this.deck!.shuffle();
+    if (cardDrawed) this.hand!.addOneCard(cardDrawed);
+  }
+}
