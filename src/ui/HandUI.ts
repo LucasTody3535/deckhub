9ba615c2d1a36d8bc81fc28dab21752dd3dac1da
Toodie <lucas.vtodt@gmail.com>
2025-10-04 23:50:08 -0300

    style: display card description when hovering card

--- src/ui/HandUI.ts
+++ src/ui/HandUI.ts
@@ -3,23 +3,40 @@ import { CardUI } from "./CardUI";
 
 export class HandUI {
   private root: HTMLElement;
+  private cardDescription: HTMLParagraphElement;
   private hand: Hand;
   private cards!: Array<CardUI>;
 
   constructor(hand: Hand, parent?: HTMLElement) {
     this.hand = hand;
     this.root = document.createElement("div");
+    this.cardDescription = document.createElement("p");
     this.root.classList.add("hand-ui");
+    this.cardDescription.classList.add("hand-ui-card-description");
     this.updateUI();
+    parent?.appendChild(this.cardDescription);
     parent?.appendChild(this.root);
   }
 
   private addEventHandler(card: CardUI, index: number) {
+    let cardDesc = this.cardDescription;
     card.onClick(() => {
       const cardRemoved = this.hand.removeCard(index);
       cardRemoved.getEffect()?.apply();
       this.cards.forEach((card) => card.removeOnClickHandler());
       this.updateUI();
+    });
+    card.onHover({
+      onMouseEnter(card) {
+        if (card.getDescription().replaceAll(" ", "") != "") {
+          cardDesc.classList.add("display-description");
+          cardDesc.innerText = card.getDescription();
+        }
+      },
+      onMouseLeave() {
+        cardDesc.classList.remove("display-description");
+        cardDesc.innerText = "";
+      },
     });
   }
 
