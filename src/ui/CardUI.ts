c558cde761614c1ff73bd79fead54d7e50651126
Toodie <lucas.vtodt@gmail.com>
2025-10-04 23:49:14 -0300

    style: impl method to set card's mouse enter/leave listeners

--- src/ui/CardUI.ts
+++ src/ui/CardUI.ts
@@ -1,10 +1,16 @@
 import type { Card } from "../core/Card";
 
+type HoverEvent = {
+  onMouseEnter: (card: Card) => void;
+  onMouseLeave: () => void;
+};
+
 export class CardUI {
   private root: HTMLElement;
   private text: HTMLParagraphElement;
   private card: Card;
   private clickHandler!: () => void;
+  private hoverHandler!: HoverEvent;
 
   constructor(parent: HTMLElement, card: Card) {
     this.root = document.createElement("div");
@@ -23,5 +29,21 @@ export class CardUI {
 
   public removeOnClickHandler() {
     this.root.removeEventListener("click", this.clickHandler);
+  }
+
+  public onHover(hoverEv: HoverEvent) {
+    this.root.addEventListener("mouseenter", () =>
+      hoverEv.onMouseEnter(this.card),
+    );
+    this.root.addEventListener("mouseleave", () => hoverEv.onMouseLeave());
+  }
+
+  public removeOnHover() {
+    this.root.removeEventListener("mouseenter", () =>
+      this.hoverHandler.onMouseEnter(this.card),
+    );
+    this.root.removeEventListener("mouseleave", () =>
+      this.hoverHandler.onMouseLeave(),
+    );
   }
 }
